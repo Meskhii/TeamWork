4b188cc4c7c8d8b8841413aaf875447f945a9e32
tsikk <tsikelashviliilia@gmail.com>
2021-07-05 20:37:51 +0400

    merge

---
+++ TeamWork/View/SecondCell/SecondCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondCell.swift
+//  TeamWork
+//
+//  Created by Ilia Tsikelashvili on 05.07.21.
+//
+
+import UIKit
+
+class SecondCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
